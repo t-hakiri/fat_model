d1ad5e8a39ce76ab0d548cd02610d850f04deaff
takuya hakiri <tyapi4@yahoo.co.jp>
2019-11-29 15:45:58 +0900

    fat_model_scope

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,3 @@
 class Blog < ApplicationRecord
-  # def self.latest(number)
-  #   order(created_at: :desc).limit(number)
-  # end
-
-  scope :latest, -> (number = 3){order(created_at: :desc).limit(number)}
+  include CommonModule
 end
